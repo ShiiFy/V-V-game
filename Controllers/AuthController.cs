251d2a8469f19d71e081838b1773693457f6c57c
user <user@WIN-CNMCE5527U8>
2025-03-18 22:28:23 +0200

    Добавьте файлы проекта.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace V_V_game.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Sing()
+        {
+            return View();
+        }
+        public ActionResult Reg()
+        {
+            return View();
+        }
+    }
+}
